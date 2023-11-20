038eb44dab540be734f2498e3f18cd5d4b337038
Nahuel Luca <nahuelluca20@gmail.com>
2023-11-20 20:36:57 -0300

    Refactor modal styles and button labels

--- components/add-product.tsx
+++ components/add-product.tsx
@@ -13,7 +13,7 @@ export default function AddProduct({
     <div
       aria-hidden="true"
       className={clsx(
-        "overflow-hidden z-50 flex fixed  bg-[hsl(0,0%,0%,0.8)] justify-center mx-auto items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full",
+        "transition-all overflow-hidden z-50 flex fixed  bg-[hsl(0,0%,0%,0.8)] justify-center items-center w-full md:inset-0 h-screen max-h-full",
         {
           block: isOpen,
           hidden: !isOpen,
@@ -22,7 +22,7 @@ export default function AddProduct({
       data-modal-backdrop="static"
       id="static-modal"
     >
-      <div className="relative p-4 w-full max-w-2xl max-h-full">
+      <div className={"relative p-4 w-full max-w-2xl max-h-full"}>
         <div className="relative bg-black border-nextGray-200 border-2 rounded-lg shadow dark:bg-gray-700">
           <div
             className="flex items-center justify-between p-4 md:p-5 border-b
@@ -54,18 +54,19 @@ export default function AddProduct({
           </div>
           <div className="flex items-center p-4 md:p-5 border-t border-nextGray-200 rounded-b dark:border-gray-600">
             <button
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-vercel-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               data-modal-hide="static-modal"
               type="button"
             >
-              I accept
+              Add
             </button>
             <button
-              className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
               data-modal-hide="static-modal"
               type="button"
+              onClick={() => setIsOpen(false)}
             >
-              Decline
+              Close
             </button>
           </div>
         </div>
