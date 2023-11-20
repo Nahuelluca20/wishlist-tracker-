8babfb5e09e5905fb872660f4a67f4f7e295b872
Nahuel Luca <nahuelluca20@gmail.com>
2023-11-19 22:09:30 -0300

    Add logo to search bar

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function SearchBar() {
-  return <nav className="w-full rounded-lg bg-nextGray-300 px-5 py-4">das</nav>;
+  return (
+    <nav className="w-full flex rounded-lg bg-nextGray-300 px-5 py-4 items-center">
+      <div>
+        <Image
+          alt="logo"
+          className="bg-white rounded-full border w-10 h-10"
+          height={40}
+          src="https://www.svgrepo.com/show/354113/nextjs-icon.svg"
+          width={40}
+        />
+      </div>
+    </nav>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     ppr: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
