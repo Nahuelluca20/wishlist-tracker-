3292fe832cc33d8d58f8b9be4a16249c9a238e5e
Nahuel Luca <nahuelluca20@gmail.com>
2023-11-26 16:26:04 -0300

    Fix onClick function to use async/await

--- components/add-product-form.tsx
+++ components/add-product-form.tsx
@@ -91,7 +91,7 @@ export default function AddProductForm({setIsOpen}: {setIsOpen: (isOpen: boolean
           className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
           data-modal-hide="static-modal"
           type="button"
-          onClick={() => setIsOpen(false)}
+          onClick={async () => await setIsOpen(false)}
         >
           Close
         </button>
